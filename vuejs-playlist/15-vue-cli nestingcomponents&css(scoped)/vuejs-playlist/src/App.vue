cb121c4a52ee1e10673caef91e14515f65d3cf3d
AlexZ33 <775136985@qq.com>
2017-06-13 17:15:45 +0800

    vue-cli nesting-components

---
+++ vuejs-playlist/15-vue-cli nestingcomponents&css(scoped)/vuejs-playlist/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <app-header></app-header>  
+    <app-jxs></app-jxs>
+    <app-footer></app-footer>  
+  </div>
+</template>
+
+<script>
+import Header from "./components/header.vue";
+import Footer from "./components/footer.vue";
+import Jxs from "./components/jxs.vue";
+
+export default {
+  components:{
+    "app-header": Header,
+    "app-footer": Footer,
+    "app-jxs": Jxs
+  },
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuejs-playlist/15-vue-cli nestingcomponents&css(scoped)/vuejs-playlist/src/components/jxs.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="jxs">
+    <ul>
+      <li v-for="jx in jxs" v-on:click="jx.show =!jx.show">
+        <h2>{{jx.name}}</h2>
+        <h3 v-show="jx.show">{{jx.specially}}</h3>
+      </li>
+    </ul>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      jxs:[
+        {name:'Ryu',specially:"Vue Components",show: false},
+        {name:'Crystal',specially:"HTML Wizardry",show: false},
+        {name:'Hitoshi',specially:"Click Events",show: false},
+        {name:'Tango',specially:"Conditionals",show: false},
+        {name:'Kami',specially:"Webpack",show: false},
+        {name:'Yoshi',specially:"Data Diggin",show: false}
+        
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+#jxs{
+  width: 100%;
+  max-width: 1200px;
+  margin: 40px auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+ul{
+  display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+  padding: 0;
+
+}
+li{
+  flex-grow: 1;
+  flex-basis: 300px;
+  text-align: center;
+  padding: 30px;
+  border: 1px solid #222;
+  margin: 10px;
+
+}
+</style>
